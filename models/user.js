ccaffaed7303fb57408a42168f162aaf2401ac74
R0ch4 <ede_rocha@hotmail.com>
2017-03-22 15:58:17 -0300

    Ajust API to consume with Angular 2

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var schema = new Schema({
     lastName: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    messages: [{ type: Schema.Types.ObjectId, ref }]
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
 });
 
 schema.plugin(mongooseUniqueValidator);
